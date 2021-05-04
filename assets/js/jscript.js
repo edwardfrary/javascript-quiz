e1a47bc1e44bfb0964a6ce8812c307ee9115b2f7
edwardfrary <edwardfrary@gmail.com>
2021-05-04 10:18:14 -0400

    Update jscript.js

--- assets/js/jscript.js
+++ assets/js/jscript.js
@@ -1,3 +1,21 @@
 var questions = [];
 var questionHeader = document.getElementById("question-header");
 var questionAnswer = document.getElementById("answer-choice");
+
+var questionsObj1 = {
+    header: "How do you declare a variable?",
+    answer1: "var 'variableName' = 'data'",
+    answer2: "'data' = var 'variableName'",
+    answer3: "variable = 'variableName', 'data'"
+};
+
+var questionsObj2 = {
+    header: "How do you assign an Element to a variable by it's Id?",
+    answer1: "var 'variableName' = document.getElementById('ElementId')",
+    answer2: "'ElementId' = 'variableName' = document.GetElementbyName"
+};
+
+questions.push(questionsObj1);
+questions.push(questionsObj2);
+
+console.log(questions);
